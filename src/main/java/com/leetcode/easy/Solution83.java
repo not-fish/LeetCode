52b54d4326d1363e61f17989e4e1038436155fab
Poko <841870543@qq.com>
2020-12-11 21:04:01 +0800

    [update] easy-83

--- src/main/java/com/leetcode/easy/Solution83.java
+++ src/main/java/com/leetcode/easy/Solution83.java
@@ -2,6 +2,17 @@ package com.leetcode.easy;
 
 /**
  * @author Peko
+ *
+ * 删除排序链表中的重复元素
+ *      给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ *
+ * 解题思路1：
+ *      循环链表，判断当前节点是否与下一节点的值相同，如果相同则把当前节点的地址换成下一节点的地址
+ *
+ * 效率：
+ *      O(N)  0ms    100%
+ *      O(1)  38MB   62.35%
+ *
  */
 public class Solution83 {
     public ListNode deleteDuplicates(ListNode head) {
